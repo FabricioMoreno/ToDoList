951c953aebc7560681781267775e361753733a3d
Fabricio <fabricio.morenonaranjo@hotmail.com>
2020-06-17 17:16:59 -0500

    Funcionalidades completadas

--- js/List.js
+++ js/List.js
@@ -1,7 +1,4 @@
 export default class List {
-    constructor(target){
-        this.target = target
-    }
     showTasks(tasks){
         (tasks.length === 0)?console.log('No hay tareas con ese nombre'):undefined;
 
@@ -9,9 +6,56 @@ export default class List {
             console.log(task)
         }
     }
-    search(valueToSearch){
+
+    searchTag(valueToSearch){
         const tasks = [...document.getElementsByClassName('task')]
         const tasksFiltered = tasks.filter(task=>task.classList[1] === valueToSearch)
         this.showTasks(tasksFiltered)
+    }
+
+    addTask(content,tag){
+        if(!content){
+            swal("Este campo no puede estar vacio","", "error");
+            return
+        }
+        const list = document.getElementById('listOfTasks')
+        const newTask = document.createElement('li')
+        let nameTag = tag || 'anonymous-tag'
+        newTask.setAttribute('class', `task ${nameTag}`)
+        newTask.innerHTML = `<div class="task_tag"><span class="task_tag-content"><i class="fas fa-tags icon-tag"></i>${nameTag}</span></div>
+                            <label class="task_content" data-btn="check" >${content}</label>
+                            <textarea class="task_content--edit">${content}</textarea>
+                            <div class="task_options">
+                                <button class="task_btn-listo" data-btn="listo">Listo</button>
+                                <button class="task_btn-edit" data-btn="edit-task">Edit</button>
+                                <button class="task_btn-delete" data-btn="delete-task">Delete</button>
+                            </div>
+                        `
+        list.insertBefore(newTask, list.childNodes[0]);
+    }
+
+    editTask(e){
+        const theActualTask = e.path[2]
+        theActualTask.classList.add('editMode')    
+    }
+
+    ready(e){
+        const theActualTask = e.path[2]
+        let contentOfTheActualTask= theActualTask.childNodes[2]
+        let contentOfTheEditTask = theActualTask.childNodes[4]
+        contentOfTheActualTask.textContent = contentOfTheEditTask.value
+        
+        theActualTask.classList.remove('editMode')
+        contentOfTheActualTask.classList.remove('check-task')
+    }
+    
+    deleteTask(e){
+        const theActualTask = e.path[2];
+        theActualTask.remove()
+    }
+
+    checkTask(e){
+        const checkThisTask = e.target
+        checkThisTask.classList.add('check-task')
     }
 }
--- js/app.js
+++ js/app.js
@@ -4,15 +4,36 @@ const father = document.getElementById('father')
 const search = document.getElementById('search')
 const list = new List()
 
+list.addTask('Esto es una nota de prueba')
 function detectComponent(e){
     const target = e.target.dataset
     
     if(target.btn === "search-tag"){
         const content = document.getElementById('toSearch').value
-        list.search(content)
+        list.searchTag(content)
     }
     if(target.btn === 'add-task'){
-        console.log('Aquiii')
+        let content = document.getElementById('taskContentToAdd')
+        const nameTag = document.getElementById('tag-name')
+        list.addTask(content.value,nameTag.value)
+        content.value = ' ' ;   
+        nameTag.value = ' '
+    }
+
+    if(target.btn === 'edit-task'){
+        list.editTask(e)
+    }
+
+    if(target.btn === 'listo'){
+        list.ready(e)
+    }
+
+    if(target.btn === 'delete-task'){
+        list.deleteTask(e)
+    }
+    
+    if(target.btn === 'check'){
+        list.checkTask(e)
     }
     
 }
@@ -21,7 +42,7 @@ function enter(e){
     const keycode = e.keycode || e.which
     if(keycode === 13){
         const valueToSearch = document.getElementById('toSearch').value
-        list.search(valueToSearch)
+        list.searchTag(valueToSearch)
     }
 }
 
